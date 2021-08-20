Fixed piece calculation bug

chess_game.py
@@ -5,15 +5,19 @@ from pydispatch import Dispatcher
 class ChessBoard:
     def __init__(self):
         self.board = [[None for _ in range(8)] for _ in range(8)]
-        self.board_state = [[0 for _ in range(8)] for _ in range(8)]
+        self.board_state = np.zeros((8, 8))
         self.point_balance = 0
         self.white_turn = True
 
     def place(self, piece: ChessPiece):
-        self.board[piece.starting_pos[0]][piece.starting_pos[1]] = piece
+        start_col = piece.starting_pos[0]
+        start_row = piece.starting_pos[1]
+        self.board[start_col][start_row] = piece
+        self.board_state[(start_col, start_row)] = piece.piece_type
+        print(self.board_state)
 
-    def calc_moves(self, piece: ChessPiece):
-        piece.reachable_squares = []
+    def calc_moves(self, piece: ChessPiece, game_state: np.ndarray):
+        reachable_squares = []
         if piece.long_range:
             found_limit = np.array([False for _ in range(len(piece.move_units))])
             dist = 1
@@ -22,10 +26,11 @@ class ChessBoard:
                 for i in range(len(found_limit)):
                     if not found_limit[i]:
                         move = piece.position + piece.move_units[i] * dist
-                        if self.verify_move(piece, move, capture=False):
-                            pass
+                        if self.verify_move(piece, move, game_state, capture=False):
+                            reachable_squares.append(move)
                         else:
-                            self.verify_capture(piece, move)
+                            if self.verify_capture(piece, move, game_state):
+                                reachable_squares.append(move)
                             found_limit[i] = True
                 dist += 1
 
@@ -33,56 +38,154 @@ class ChessBoard:
 
             base_move = piece.position + piece.move_units[0]
 
-            self.verify_move(piece, base_move, capture=False)
+            if self.verify_move(piece, base_move, game_state, capture=False):
+                reachable_squares.append(base_move)
 
-            if (piece.position == piece.starting_pos).all():
-                self.verify_move(piece, base_move + piece.move_units[0], capture=False)
+            if (piece.position == piece.starting_pos).all() and \
+                    self.verify_move(piece, base_move + piece.move_units[0], game_state, capture=False):
+                reachable_squares.append(base_move + piece.move_units[0])
 
-            self.verify_capture(piece, base_move + np.array([1, 0]))
-            self.verify_capture(piece, base_move + np.array([-1, 0]))
+            if self.verify_capture(piece, base_move + np.array([1, 0]), game_state):
+                reachable_squares.append(base_move + np.array([1, 0]))
+            if self.verify_capture(piece, base_move + np.array([-1, 0]), game_state):
+                reachable_squares.append(base_move + np.array([-1, 0]))
 
         else:
             for vec in piece.move_units:
                 move = piece.position + np.array(vec)
-                self.verify_move(piece, move, capture=True)
+                if self.verify_move(piece, move, game_state, capture=True):
+                    reachable_squares.append(move)
 
-    def verify_move(self, piece: ChessPiece, move: np.ndarray, capture: bool):
+        return reachable_squares
+
+    def verify_move(self, piece: ChessPiece, move: np.ndarray, game_state: np.ndarray, capture: bool):
         if move[0] < 0 or move[1] < 0:
             return False
         try:
-            if self.board[move[0]][move[1]] is None:
-                piece.reachable_squares.append(move)
-                return True
+            if game_state[move[0], move[1]] == 0:
+                return self.verify_safe(piece, move)
         except IndexError:
             return False
 
         if capture:
-            return self.verify_capture(piece, move)
+            return self.verify_capture(piece, move, game_state)
 
         return False
 
-    def verify_capture(self, piece: ChessPiece, move: np.ndarray):
-        # if move[0] < 0 or move[1] < 0:
-        #     return False
+    def verify_capture(self, piece: ChessPiece, move: np.ndarray, game_state: np.ndarray):
+        if move[0] < 0 or move[1] < 0:
+            return False
         try:
-            target_piece = self.board[move[0]][move[1]]
-            if isinstance(target_piece, ChessPiece):
-                if piece.white ^ target_piece.white:
-                    piece.reachable_squares.append(move)
-                    return True
-                return False
+            target_piece = game_state[move[0], move[1]]
+            if target_piece != 0:
+                return piece.piece_type * target_piece < 0 and self.verify_safe(piece, move)
         except IndexError:
             return False
         return False
 
     def verify_safe(self, piece: ChessPiece, move: np.ndarray):
-        pass
+
+        print("Checking for checking...")
+
+        test_board = self.board_state.copy()
+        # print(piece.move_units)
+        test_board[move[0]][move[1]] = piece.piece_type
+        test_board[piece.position[0]][piece.position[1]] = 0
+        # print(test_board)
+
+        if piece.white:
+            king_value = 6
+            pawn_value = -1
+            bishop_value = -2
+            knight_value = -3
+            rook_value = -4
+            queen_value = -5
+        else:
+            king_value = -6
+            pawn_value = 1
+            bishop_value = 2
+            knight_value = 3
+            rook_value = 4
+            queen_value = 5
+
+        test_units = [np.array(vec) for vec in [[0, 1], [0, -1], [1, 0], [-1, 0], [1, 1], [1, -1], [-1, 1], [-1, -1]]]
+
+        knight_units = [[1, 2], [1, -2], [-1, 2], [-1, -2], [2, 1], [2, -1], [-2, 1], [-2, -1]]
+
+        try:
+            king_pos = np.array([arr[0] for arr in np.where(test_board == king_value)])
+        except Exception:
+            print("safety exception")
+            return False
+
+        # print(king_pos)
+
+        for diag in [np.array([1, 1]), np.array([1, -1])]:
+            # print("For loop")
+            move = king_pos + diag * pawn_value
+            try:
+                if test_board[move[0], move[1]] == pawn_value:
+                    # print("Condition 1")
+                    return False
+            except Exception:
+                print("Error!")
+                pass
+
+        found_limit = np.array([False for _ in range(len(test_units))])
+        print(found_limit)
+        dist = 1
+
+        while not np.all(found_limit):
+            print("Cycling...")
+            for i, unit in enumerate(test_units):
+                if not found_limit[i]:
+                    move = king_pos + unit * dist
+
+                    if move[0] < 0 or move[1] < 0:
+                        found_limit[i] = True
+                    else:
+                        try:
+                            if test_board[move[0], move[1]] == 0:
+                                continue
+                            elif test_board[move[0], move[1]] * piece.piece_type > 0:
+                                found_limit[i] = True
+                            else:
+                                if test_board[move[0], move[1]] == queen_value:
+                                    # print("Condition 2")
+                                    return False
+                                elif np.prod(move) == 0 and test_board[move[0], move[1]] == rook_value:
+                                    # print("Condition 3")
+                                    return False
+                                elif np.prod(move) != 0 and test_board[move[0], move[1]] == bishop_value:
+                                    # print("Condition 4")
+                                    return False
+                                found_limit[i] = True
+                        except IndexError:
+                            found_limit[i] = True
+            dist += 1
+            print(dist)
+
+        for unit in knight_units:
+            move = king_pos + unit
+            if move[0] < 0 or move[1] < 0:
+                continue
+            try:
+                if test_board[move[0], move[1]] == knight_value:
+                    # print("Condition 5")
+                    return False
+            except IndexError:
+                continue
+
+        # print("It's safe.")
+        return True
 
     def move_piece(self, piece: ChessPiece, move: np.ndarray):
         if np.asarray(piece.reachable_squares).__contains__(move):
             prev_pos = piece.position
             self.board[move[0]][move[1]] = piece
             self.board[prev_pos[0]][prev_pos[1]] = None
+            self.board_state[move[0], move[1]] = piece.piece_type
+            self.board_state[prev_pos[0], prev_pos[1]] = 0
             piece.position = move
             self.white_turn = not self.white_turn
 
@@ -93,6 +196,8 @@ class ChessBoard:
             prev_pos = piece.position
             self.board[move[0]][move[1]] = piece
             self.board[prev_pos[0]][prev_pos[1]] = None
+            self.board_state[move[0], move[1]] = piece.piece_type
+            self.board_state[prev_pos[0], prev_pos[1]] = 0
             piece.position = move
             self.white_turn = not self.white_turn
 

chess_piece.py
@@ -3,8 +3,8 @@ import numpy as np
 
 # TODO: Implement move legality based on checking the king
 class ChessPiece:
-    def __init__(self, point_value: int, starting_pos: np.ndarray, long_range: bool, white: bool, piece_type: str):
-        self.point_value = point_value
+    def __init__(self, point_value: int, starting_pos: np.ndarray, long_range: bool, white: bool, piece_type: int):
+        self.point_value = point_value if white else -point_value
         self.starting_pos = starting_pos
         self.position = starting_pos
         self.reachable_squares = []
@@ -12,9 +12,6 @@ class ChessPiece:
         self.long_range = long_range
         self.white = white
         self.piece_type = piece_type
-
-        if not white:
-            self.point_value *= -1
 
 
     # def verify_move(self, square, capture=False):
@@ -42,11 +39,9 @@ class ChessPiece:
 # TODO: Implement en passant rule for pawns
 class Pawn(ChessPiece):
     def __init__(self, starting_pos: (int, int), white: bool):
-        super().__init__(point_value=1, starting_pos=starting_pos, long_range=False, white=white, piece_type="pawn")
-        if self.white:
-            self.move_units = [np.array([0, 1])]
-        else:
-            self.move_units = [np.array([0, -1])]
+        super().__init__(point_value=1, starting_pos=starting_pos, long_range=False, white=white,
+                         piece_type=1 if white else -1)
+        self.move_units = [np.array([0, 1])] if white else [np.array([0, -1])]
 
     # def calc_moves(self):
     #     self.reachable_squares = []
@@ -70,7 +65,8 @@ class Pawn(ChessPiece):
 
 class Bishop(ChessPiece):
     def __init__(self, starting_pos, white):
-        super().__init__(point_value=3, starting_pos=starting_pos, long_range=True, white=white, piece_type="bishop")
+        super().__init__(point_value=3, starting_pos=starting_pos, long_range=True, white=white,
+                         piece_type=2 if white else -2)
         self.move_units = [np.array(vec) for vec in [[1, 1], [1, -1], [-1, 1], [-1, -1]]]
 
     # def calc_moves(self):
@@ -94,7 +90,8 @@ class Bishop(ChessPiece):
 
 class Knight(ChessPiece):
     def __init__(self, starting_pos, white):
-        super().__init__(point_value=3, starting_pos=starting_pos, long_range=False, white=white, piece_type="knight")
+        super().__init__(point_value=3, starting_pos=starting_pos, long_range=False, white=white,
+                         piece_type=3 if white else -3)
         self.move_units = [[1, 2], [1, -2], [-1, 2], [-1, -2], [2, 1], [2, -1], [-2, 1], [-2, -1]]
 
     # def calc_moves(self):
@@ -108,7 +105,8 @@ class Knight(ChessPiece):
 
 class Rook(ChessPiece):
     def __init__(self, starting_pos, white):
-        super().__init__(point_value=5, starting_pos=starting_pos, long_range=True, white=white, piece_type="rook")
+        super().__init__(point_value=5, starting_pos=starting_pos, long_range=True, white=white,
+                         piece_type=4 if white else -4)
         self.move_units = [np.array(vec) for vec in [[0, 1], [0, -1], [1, 0], [-1, 0]]]
 
     # def calc_moves(self):
@@ -132,7 +130,8 @@ class Rook(ChessPiece):
 
 class Queen(ChessPiece):
     def __init__(self, starting_pos, white):
-        super().__init__(point_value=9, starting_pos=starting_pos, long_range=True, white=white, piece_type="queen")
+        super().__init__(point_value=9, starting_pos=starting_pos, long_range=True, white=white,
+                         piece_type=5 if white else -5)
         self.move_units = [np.array(vec) for vec in
                            [[0, 1], [0, -1], [1, 0], [-1, 0], [1, 1], [1, -1], [-1, 1], [-1, -1]]]
 
@@ -157,7 +156,8 @@ class Queen(ChessPiece):
 
 class King(ChessPiece):
     def __init__(self, starting_pos, white):
-        super().__init__(point_value=np.inf, starting_pos=starting_pos, long_range=False, white=white, piece_type="king")
+        super().__init__(point_value=np.inf, starting_pos=starting_pos, long_range=False, white=white,
+                         piece_type=6 if white else -6)
         self.move_units = [np.array(vec) for vec in
                            [[0, 1], [0, -1], [1, 0], [-1, 0], [1, 1], [1, -1], [-1, 1], [-1, -1]]]
 

game_gui.py
@@ -4,6 +4,8 @@ from PIL import ImageTk, Image
 from chess_piece import *
 from chess_game import ChessBoard
 
+type_dict = {1: "pawn", 2: "bishop", 3: "knight", 4: "rook", 5: "queen", 6: "king"}
+
 class Application(tk.Frame):
 
     def __init__(self, master=None):
@@ -69,12 +71,13 @@ class Application(tk.Frame):
                 self.add_piece(Pawn(np.array([i, pawn_row]), white))
 
     def add_piece(self, piece: ChessPiece):
+
         filename = "./assets/pieces/"
         if piece.white:
             filename += "white_"
         else:
             filename += "black_"
-        filename += piece.piece_type
+        filename += type_dict[abs(piece.piece_type)]
 
         if (piece.position[0] + piece.position[1]) % 2 == 0:
             filename += "_dark.png"
@@ -94,7 +97,7 @@ class Application(tk.Frame):
     def show_moves(self, piece: ChessPiece):
         for board_piece in self.piece_dict.keys():
             self.piece_dict[board_piece].bind("<ButtonPress-1>", lambda event, board_piece=board_piece: self.show_moves(board_piece))
-        self.board.calc_moves(piece)
+        piece.reachable_squares = self.board.calc_moves(piece, self.board.board_state)
         for label in self.move_labels:
             label.destroy()
         self.move_labels = []
@@ -119,7 +122,7 @@ class Application(tk.Frame):
                     filename += "white_"
                 else:
                     filename += "black_"
-                filename += target_piece.piece_type
+                filename += type_dict[abs(target_piece.piece_type)]
 
                 if (target_piece.position[0] + target_piece.position[1]) % 2 == 0:
                     filename += "_dark_highlight.png"
@@ -171,7 +174,7 @@ class Application(tk.Frame):
             filename += "white_"
         else:
             filename += "black_"
-        filename += piece.piece_type
+        filename += type_dict[abs(piece.piece_type)]
 
         if (piece.position[0] + piece.position[1]) % 2 == 0:
             filename += "_dark.png"